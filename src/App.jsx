aa27b84cd705c330a7e23c57de4b310674332cce
mehedi hasan <mhshohan04@gmail.com>
2025-06-11 09:33:40 +0600

    Load coffee data from MongoDB using React Router loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { useLoaderData } from 'react-router'
 import './App.css'
 
 function App() {
-
+  const coffees = useLoaderData();
   return (
    <>
      <button className="btn btn-soft btn-accent">Accent</button>
       <h2 className='text-green-400'>coffee store client side</h2>
-    
+      <h1>my coffee: {coffees.length}</h1>
   </>
   )
 }
